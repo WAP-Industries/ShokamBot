1726d66722ba878fe95a2f9b61dc10b1a6c99314
WAP Industries <141229412+WAP-Industries@users.noreply.github.com>
2023-09-30 22:31:40 +0800

    Add files via upload

--- sch.bat
+++ sch.bat
@@ -6,7 +6,7 @@ cd %~dp0
 set "taskname=ShokamBot"
 set "tasksettings=$TaskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable;"
 
-schtasks /create /tn "%taskname%" /tr "%cd%\run.bat" /sc daily /st "14:00"
+schtasks /create /tn "%taskname%" /tr "%cd%\run.bat" /sc hourly /mo 6 /st 00:00:00 /f
 powershell -command %tasksettings%"Set-ScheduledTask -TaskName %taskname% -Settings $TaskSettings"
 
 endlocal
